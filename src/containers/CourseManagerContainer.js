09062c603fc43beddef373874d11f186e5d31b42
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-02-08 16:33:45 -0500

    mapped table and grid view

--- src/containers/CourseManagerContainer.js
+++ src/containers/CourseManagerContainer.js
@@ -2,9 +2,15 @@ import React from "react";
 import CourseManagerNavbarComponent from "../components/CourseManager/CourseManagerNavbarComponent";
 import CourseTableComponent from "../components/CourseManager/CourseTableComponent";
 import CourseGridComponent from "../components/CourseManager/CourseGridComponent";
-import {createCourse, deleteCourse, findAllCourses, updateCourse, findCourseById} from "../services/CourseService"
+import {
+    createCourse,
+    deleteCourse,
+    findAllCourses,
+    findCourseById,
+    updateCourse
+} from "../services/CourseService"
 import CourseEditorComponent from "../components/CourseEditor/CourseEditorComponent";
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import {Router} from "react-router";
 import createHistory from 'history/createBrowserHistory';
 
@@ -27,14 +33,7 @@ class CourseManagerContainer extends React.Component {
         document.title = 'Course Manager | Whiteboard';
         await this.loadAllCourses();
     };
-    getCourseTitleById =  (id) => {
-        console.log("asked", id, this.state.courses);
-        let res = this.state.courses.filter((course) => {
-           return course._id === id;
-        });
-        console.log("giving", res[0]);
-        return res[0];
-    };
+
     update = async (courseId, course) => {
         await updateCourse(courseId, course);
         await this.loadAllCourses();
@@ -87,78 +86,102 @@ class CourseManagerContainer extends React.Component {
     };
 
     render() {
-        return (
+        return <div>
+            <Router history={history}>
+                <Route
+                    path="/"
+                    exact={true}
+                    render={(props) =>
+                        <Redirect to="/table"/>
 
-            <div>
-                <Router history={history}>
-                    <Route
-                        path="/course-editor/:courseId"
-                        exact={true}
-                        render={(props) =>
-                            <CourseEditorComponent
-                                findCourseById={findCourseById}
-                                courseId={props.match.params.courseId}
-                                {...props}/>
-                        }/>
+                    }/>
+                    
+                <Route
+                    path="/course-editor/:courseId"
+                    exact={true}
+                    render={(props) =>
+                        <CourseEditorComponent
+                            findCourseById={findCourseById}
+                            courseId={props.match.params.courseId}
+                            {...props}/>
+                    }/>
 
-                    <Route
-                        path="/"
-                        exact={true}
-                        render={(props) =>
+                <Route
+                    path="/grid"
+                    exact={true}
+                    render={(props) => {
+                        return <div>
                             <div>
-                                 <div>
-                                     <CourseManagerNavbarComponent add={this.addCourse}/>
-                                     <nav className="navbar navbar-light bg-light">
-                                         <div className="container-fluid">
-                                             <div className="row justify-content-end w-100">
-                                                 {this.state.layout === 'table' &&
-                                                  <div className="col-1 mx-1">
-                                                      <button type="button" onClick={this.toggle}
-                                                              className="btn">
-                                                          <i className="fas fa-th fa-lg float-right wbdv-button wbdv-grid-layout"/>
-                                                      </button>
-                                                  </div>}
-                                                 {this.state.layout === 'grid' &&
-                                                  <div className="col-1 mx-1">
-                                                      <button type="button" onClick={this.toggle}
-                                                              className="btn">
-                                                          <i className="fas fa-lg fa-list-ul float-right wbdv-button wbdv-list-layout"/>
-                                                      </button>
-                                                  </div>}
-                                                 <div className="col-1 mx-1">
-                                                     <button type="button"
-                                                             className="btn wbdv-header wbdv-sort">
-                                                         <i className="fas fa-lg fa-sort-numeric-down float-right"/>
-                                                     </button>
-                                                 </div>
-                                             </div>
-                                         </div>
-                                     </nav>
-
-                                     {this.state.layout === 'table' &&
-                                      <CourseTableComponent {...props}
-                                                            courses={this.state.courses}
-                                                            deleteCourse={this.deleteCourse}
-                                                            updateCourse={this.update}
-                                                            formatDate={this.formatDate}/>}
-
-                                     {this.state.layout === 'grid' &&
-                                      <CourseGridComponent
-                                          {...props}
-                                          courses={this.state.courses}
-                                          deleteCourse={this.deleteCourse}
-                                          updateCourse={this.update}
-                                          formatDate={this.formatDate}/>}
-
-                                 </div>
-
+                                <CourseManagerNavbarComponent add={this.addCourse}/>
+                                <nav className="navbar navbar-light bg-light">
+                                    <div className="container-fluid">
+                                        <div className="row justify-content-end w-100">
+                                            <div className="col-1 mx-1">
+                                                <button type="button"
+                                                        onClick={() => props.history.push(
+                                                            "/table")}
+                                                        className="btn">
+                                                    <i className="fas fa-lg fa-list-ul float-right wbdv-button wbdv-list-layout"/>
+                                                </button>
+                                            </div>
+                                            <div className="col-1 mx-1">
+                                                <button type="button"
+                                                        className="btn wbdv-header wbdv-sort">
+                                                    <i className="fas fa-lg fa-sort-numeric-down float-right"/>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </nav>
+                                <CourseGridComponent
+                                    {...props}
+                                    courses={this.state.courses}
+                                    deleteCourse={this.deleteCourse}
+                                    updateCourse={this.update}
+                                    formatDate={this.formatDate}/>
                             </div>
+                        </div>
+                    }}/>
+                {<Route
+                    path="/table"
+                    exact={true}
+                    render={(props) => {
+                        return <div>
+                            <div>
+                                <CourseManagerNavbarComponent add={this.addCourse}/>
+                                <nav className="navbar navbar-light bg-light">
+                                    <div className="container-fluid">
+                                        <div className="row justify-content-end w-100">
+                                            <div className="col-1 mx-1">
+                                                <button type="button"
+                                                        onClick={() => props.history.push(
+                                                            "/grid")}
+                                                        className="btn">
+                                                    <i className="fas fa-th fa-lg float-right wbdv-button wbdv-grid-layout"/>
+                                                </button>
+                                            </div>
+                                            <div className="col-1 mx-1">
+                                                <button type="button"
+                                                        className="btn wbdv-header wbdv-sort">
+                                                    <i className="fas fa-lg fa-sort-numeric-down float-right"/>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </nav>
+                                <CourseTableComponent {...props}
+                                                      courses={this.state.courses}
+                                                      deleteCourse={this.deleteCourse}
+                                                      updateCourse={this.update}
+                                                      formatDate={this.formatDate}/>
+                            </div>
+                        </div>
+                    }}/>
+                }
 
-                        }/>
-                </Router>
+            </Router>
 
-            </div>
-        )
+        </div>
     }
 }
 
