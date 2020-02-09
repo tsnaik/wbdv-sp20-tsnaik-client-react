abbeb0241f6c86182c69f4a8faf1ee25ffd5773d
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-02-09 18:49:57 -0500

    hid the widget unless a topic is selected

--- src/components/CourseEditor/WidgetListComponent.js
+++ src/components/CourseEditor/WidgetListComponent.js
@@ -1,12 +1,15 @@
 import React from "react";
+import {connect} from "react-redux";
 
-const WidgetListComponent = ({widgets}) =>
-    <div>
-        <div className="mt-1 border rounded">
-            <div className="row mx-1 my-2">
-                <div className="col">
-                    <h5 className="wbdv-widget-header mt-2 align-center">Heading Widget</h5>
-                    <span className="float-md-right form-inline">
+class WidgetListComponent extends React.Component {
+    render() {
+        return ((this.props.currentModuleId && this.props.currentLessonId
+                && this.props.currentTopicId) && <div>
+                <div className="mt-1 border rounded">
+                    <div className="row mx-1 my-2">
+                        <div className="col">
+                            <h5 className="wbdv-widget-header mt-2 align-center">Heading Widget</h5>
+                            <span className="float-md-right form-inline">
                         <button className="btn btn-info m-1"><i
                             className="fas fa-arrow-down"/></button>
                         <button className="btn btn-info m-1"><i
@@ -17,31 +20,55 @@ const WidgetListComponent = ({widgets}) =>
                         <button className="btn btn-danger m-1"><i
                             className="fas fa-times"/></button>
                     </span>
+                        </div>
+                    </div>
+                    <div className="row mx-1 my-2">
+                        <div className="col">
+                            <div className="form-group">
+                                <input type="text" className="form-control"
+                                       placeholder="Heading text"
+                                       value="Heading text"/>
+                            </div>
+                            <div className="form-group">
+                                <select className="custom-select">
+                                    <option value="heading1">Heading 1</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control"
+                                       placeholder="Widget Name"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mx-1 my-2">
+                        <div className="col">
+                            <h5>Preview</h5>
+                            <h1>Heading text</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row mx-1 my-2">
-                <div className="col">
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Heading text"
-                               value="Heading text"/>
-                    </div>
-                    <div className="form-group">
-                        <select className="custom-select">
-                            <option value="heading1">Heading 1</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Widget Name"/>
-                    </div>
-                </div>
-            </div>
-            <div className="row mx-1 my-2">
-                <div className="col">
-                    <h5>Preview</h5>
-                    <h1>Heading text</h1>
-                </div>
-            </div>
-        </div>
-    </div>;
+        )
+    }
+}
 
-export default WidgetListComponent;
+const stateToPropertyMapper = (state) => {
+    return {
+        topics: state.topics.topics,
+        currentLessonId: state.lessons.currentLessonId,
+        currentModuleId: state.modules.currentModuleId,
+        currentTopicId: state.topics.currentTopicId
+
+    }
+};
+
+const dispatchToPropertyMapper = (dispatch) => {
+    return {
+        //TODO implement
+    }
+};
+
+export default connect(
+    stateToPropertyMapper,
+    dispatchToPropertyMapper)
+(WidgetListComponent)
