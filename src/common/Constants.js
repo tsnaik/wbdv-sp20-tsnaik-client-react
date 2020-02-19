3da536eddc66c866011dd2ab0b793952272bd5c6
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-02-19 18:54:17 -0500

    changed url to heroku

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -2,7 +2,6 @@ export const API_URL_COURSES = "https://wbdv-generic-server.herokuapp.com/api/ts
 export const API_URL_MODULES = "https://wbdv-generic-server.herokuapp.com/api/tsnaik/modules";
 export const API_URL_LESSONS = "https://wbdv-generic-server.herokuapp.com/api/tsnaik/lessons";
 export const API_URL_TOPICS = "https://wbdv-generic-server.herokuapp.com/api/tsnaik/topics";
-export const API_URL_TOPICS_NEW = "http://localhost:8080/api/topics";
+export const API_URL_TOPICS_NEW = "https://wbdv-sp20-tsnaik-server-java.herokuapp.com/api/topics";
 
-export const API_URL_WIDGETS = "http://localhost:8080/api/widgets"; //TODO change in
-                                                                            // production
+export const API_URL_WIDGETS = "https://wbdv-sp20-tsnaik-server-java.herokuapp.com/api/widgets";
